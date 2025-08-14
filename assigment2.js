91080a908e641fe74868a3d0cb5009cfd3e97663
Jagjot0303 <139967512+jagjot0303@users.noreply.github.com>
2025-08-14 10:28:55 +0530

    Add files via upload

---
+++ assigment2.js
@@ -0,0 +1,99 @@
+//question1
+// let a=90;
+// if(a>=60)
+// {
+//     console.log("grade A")
+// }
+// else if(a>=50)
+// {
+//     console.log("grade B")
+// }
+// else if(a>=40)
+// {
+//     console.log("grade C")
+// }
+// else if(a>=30)
+// {
+//     console.log("garde D")
+// }
+// else{
+//     console.log("fail grade E")
+// }
+
+//question 2
+// let a="D";
+// if(a==="P"||a==="p")
+// {
+//     console.log("PrepBytes")
+// }
+// else if(a==="Z"||a==="z")
+// {
+//     console.log("Zenith")
+// }
+// else if(a==="E"||a==="e")
+// {
+//     console.log("ExpertCoder")
+// }
+// else if(a==="D"||a==="d")
+// {
+//     console.log("Data structure")
+// }
+
+//question 3
+// let a=50;
+// let b=50;
+// let c=50;
+// if(a===b && b===c)
+// {
+//     console.log("-1")
+// }
+// else if(a>=b && a>=c)
+// {
+//     console.log(a)
+// }
+// else if(b>=a && b>=c)
+// {
+//     console.log(b)
+// }
+// else
+// {
+//     console.log(c)
+// }
+
+//question 4
+// let x=10;
+// let y=20;
+// let z=30;
+// if((x>y && x<z)||(x>z && x<y))
+// {
+// console.log(x);
+// }
+// else if((y>x && y<z) || (y>z && y<x))
+// {
+//     console.log(y);
+// }
+// else
+// {
+//     console.log(z)
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
